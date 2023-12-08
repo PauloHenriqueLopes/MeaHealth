ec9cdf5d57a7c8ce48d47b0b6e9430cfaab65eb1
PauloHenriqueLopes <144734516+PauloHenriqueLopes@users.noreply.github.com>
2023-12-08 12:30:37 -0300

    academias

--- codigo/assets/js/localizacao_academias.js
+++ codigo/assets/js/localizacao_academias.js
@@ -152,7 +152,7 @@
     
 
     function fetchReviews() {
-      return fetch('./assets/js/ratingdata.json')
+      return fetch('./ratingdata')
         .then(response => {
           if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
